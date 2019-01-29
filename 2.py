425a727ad35f1b206e19f9aca5ce7b1facaec4ac
fastz123 <fastz123@naver.com>
2019-01-29 18:12:57 +0900

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,16 @@
+def f(x,y):
+    return x+y
+
+print(f(1,4))
+
+
+f = lambda x,y : x+y
+print(f(1,4))
+
+f = lambda x : x **2
+print(f(2))
+
+f = lambda x : x/2
+print(f(3))
+
+print( (lambda x : x+1) (5))
